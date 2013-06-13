3932c6f72ed3dc6730cfdf5a468e2ae98adf3b4f
Tomer Weller <tomer.weller@gmail.com>
2013-06-13 14:29:58 +0300

    add timer

--- src/com/ubi/cyclo/MainActivity.java
+++ src/com/ubi/cyclo/MainActivity.java
@@ -5,34 +5,68 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements LocationListener{
     private static final String TAG = "CYCLO";
     private LocationManager mLocationManager;
+    private long startTime;
+    private TextView mTimerLabel;
+    private Handler mHandler;
+    private boolean isStarted;
+
+    private Runnable timerUpdater = new Runnable() {
+        @Override
+        public void run() {
+            updateTimer();
+        }
+    };
+
+    private void updateTimer(){
+        long timer = System.currentTimeMillis() - startTime;
+        mTimerLabel.setText((int)(timer/1000)+"s");
+        mHandler.postDelayed(timerUpdater, 1000);
+    }
 
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cyclo_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        Log.d(TAG, "Started.");
+        mHandler = new Handler();
+        isStarted = false;
 
-        //request location updates
+        mTimerLabel = (TextView)findViewById(R.id.timerLabel);
+        View rootView = findViewById(android.R.id.content);
+        rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isStarted) onBegin();
+            }
+        });
+    }
+
+    private void onBegin(){
+        Log.d(TAG, "onBegin()");
+        isStarted=true;
+        startTime = System.currentTimeMillis();
 //        mLocationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
 //        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-
-        //mock locations
         new MockLocationHandler(this, this).start();
+        updateTimer();
     }
 
     @Override
     protected void onDestroy() {
-        mLocationManager.removeUpdates(this);
+//        mLocationManager.removeUpdates(this);
         super.onDestroy();
     }
 
